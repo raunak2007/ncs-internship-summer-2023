83d81ab2adfe5ed3bd76237449c93ae7b61045e0
Raunak Mondal <spmondal08@gmail.com>
2023-07-09 17:36:47 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your component hierarchy and UI structure */}
+      <header>
+        <h1>Welcome to My App!</h1>
+      </header>
+      <main>
+        {/* Other components and content */}
+      </main>
+      <footer>
+        {/* Footer content */}
+      </footer>
+    </div>
+  );
+}
+
+export default App;
